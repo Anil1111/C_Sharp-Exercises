cadd542fce8c54fa8275611978456e6f9adb6f6e
Hunor85 <hunor.vadasz@gmail.com>
2019-01-07 00:51:10 +0100

    Commit
    Commit

---
+++ Invoice_class/Invoice_Class/Invoice.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Invoice_Class
+{
+    public class Invoice
+    {
+
+        private int _qty;
+        private decimal _price;
+        public string PartNumber { get; set; }
+        public string PartDescr { get; set; }
+
+        public Invoice(string partNumber, string partDescr, int qty, decimal price)
+        {
+            PartNumber = partNumber;
+            PartDescr = partDescr;
+            Qty = qty;
+            Price = price;
+        }
+
+        public int Qty 
+        { 
+            get 
+            {
+                return _qty;
+            }
+            set 
+            {
+                if (value > 0)
+                    _qty = value;
+                else
+                    _qty = 1;  
+            }
+        }
+
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value > 0)
+                    _price = value;
+                else
+                    _price = 1;
+                         
+            }
+        }
+
+        public decimal GetInvoiceAmount()
+        {
+            return Qty * Price;
+        }
+    }
+}
---
+++ Invoice_class/Invoice_Class/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Invoice_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string partNumber, partDescr;
+            int qty;
+            decimal price;
+
+            Console.WriteLine("Please enter part number ");
+            partNumber = Console.ReadLine();
+
+            Console.WriteLine("Please enter part description ");
+            partDescr = Console.ReadLine();
+
+            Console.WriteLine("Please enter quantity ");
+            qty = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter part number ");
+            price = Convert.ToDecimal(Console.ReadLine());
+
+            Invoice invoice = new Invoice(partNumber, partDescr, qty, price);
+
+            Console.WriteLine("Your order is: ");
+            Console.WriteLine("Part numner is: {0}", invoice.PartNumber);
+            Console.WriteLine("Part description: {0}", invoice.PartDescr);
+            Console.WriteLine("Quantity: {0}", invoice.Qty);
+            Console.WriteLine("Price: {0}", invoice.Price);
+            Console.WriteLine("Total: {0}", invoice.GetInvoiceAmount());
+
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
